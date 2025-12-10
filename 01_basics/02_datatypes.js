21eb4289a4eafdd9c0fbf4796c2c008abcb0d0a5
Prathamesh Mhettar <prathmeshmhettar@gmail.com>
2025-12-10 15:19:13 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //make all code follow new js version it is default in new generation
+// alert(3+3) we can not use browser we just use node.js so it can not run
+console.log(3+3)
+
+console.log("prathamesh")
+
+// there are speceficaly we can use two documentation
+// 1. MDN
+// 2. ecma tc39.es
+
+/*
+data types
+number = 2 to power of 53
+bigint
+string = ""
+boolean = true/false
+null = standalone value
+undefined
+object
+symbol => unique used in react in large scale
+
+*/
+
+console.log(typeof null); // give output object
+console.log(typeof undefined); // give output undefined
