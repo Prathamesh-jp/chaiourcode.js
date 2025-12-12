804f5e81fcd09e94e9cecc97b1cd2980a16da614
Prathamesh Mhettar <prathmeshmhettar@gmail.com>
2025-12-12 12:26:39 +0000

    conversionoperations

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,24 @@
+//let score = "33" // output of undefine = nan , 33abc=nan , null = 0, boolean true = 1
+
+// console.log(typeof score);
+
+// console.log(typeof (score));
+
+// let valueNumber = Number (score);
+
+// console.log(typeof valueNumber);
+
+// console.log(valueNumber);
+
+// let valueoff = 1
+// let convertboolean = Boolean (valueoff)  // if empty string "" = false, if "prathamesh"= true,
+
+// console.log(convertboolean);
+
+
+let someNumber= 23;
+
+convertNumber = String (23); // this can print 23 but the typeof this number is coverted as String
+console.log(typeof convertNumber);
+console.log(convertNumber);
+
